0799927fe570dd874bbdeddbe6dc57c621062f8e
Manuel Romero <manuelbrc@gmail.com>
2014-05-05 10:40:55 -0500

    fix

--- js/ajax.js
+++ js/ajax.js
@@ -2,54 +2,7 @@ $.ajaxSetup ({
     cache: false
 });
 
-<<<<<<< HEAD
 
-=======
-var requestHandler={
-    params:{},
-    container:'.container',
-    Controller:'',
-    Action:'',
-    init:function(form,Controller,Action,dpost,successCallback,errorCallback,finalCallback){
-        this.form=form;
-        if(successCallback!='')this.successCallback=successCallback;
-        if(errorCallback!='')this.errorCallback=errorCallback;
-        if(finalCallback!='')this.finalCallback=finalCallback;
-        this.params=(form=='')?dpost:jQuery.parseJSON(JSON.stringify($('#'+form).serializeObject()));
-        this.Controller=Controller;
-        this.Action=Action;
-    },
-    doRequest:function(){
-        $.ajax({
-            type: "POST",
-            url: 'callHandlerMVC.php',
-            dataType: 'json',
-            data: {
-                data:this.params,
-                Controlador:this.Controller,
-                Accion:this.Action
-            },
-            statusCode:{
-                404:function(){
-                    console.log('No se encontro la madre esa');
-                }
-            },
-            success: this.successCallback,
-            error:this.errerrorCallback,
-            complete:this.finalCallback
-        });
-    },
-    successCallback:function(){
-        
-    },
-    errorCallback:function(){
-        console.log('algo trono');
-    },
-    finalCallback:function(){
-        console.log('y termino esta madre');
-    }
-};
->>>>>>> 7e4eb7d1d8cc773c4c0c3e44ae328e6921103f4b
 
 var loadLink=function(event){
     event.preventDefault();
@@ -57,7 +10,6 @@ var loadLink=function(event){
     $(".container").load(url);
     return false;
 };
-<<<<<<< HEAD
 var responsiveMenu=function(event){
     event.preventDefault();
     $("nav ul").slideToggle();
@@ -70,7 +22,7 @@ $(window).resize(function(){
     }
 });
 var init=function(){
-	$("a").on("click",loadLink);
+    $("a").on("click",loadLink);
     $("#pullMenu").on("click",responsiveMenu);
     
 };
@@ -80,29 +32,6 @@ var posicionarMenu=function() {
 
     var altura_del_header = $('header').outerHeight(true);
     var altura_del_menu = $('nav').outerHeight(true);
-=======
-var init=function(){
-	$("a").on("click",loadLink);
-    /*
-    var peticion=Object.create(requestHandler);
-    peticion.type='al 100';
-    peticion.doRequest();
-    */
-};
-$(document).on("ready",init);
-
-posicionarMenu();
-
-$(window).scroll(function() {    
-    posicionarMenu();
-});
-
-function posicionarMenu() {
-
-    var altura_del_header = $('header').outerHeight(true);
-    var altura_del_menu = $('nav').outerHeight(true);
-
->>>>>>> 7e4eb7d1d8cc773c4c0c3e44ae328e6921103f4b
     if ($(window).scrollTop() >= altura_del_header){
         $('nav').addClass('fixed');
         $('.container').css('margin-top', (altura_del_menu) + 'px');
@@ -110,13 +39,9 @@ function posicionarMenu() {
         $('nav').removeClass('fixed');
         $('.container').css('margin-top', '0');
     }
-<<<<<<< HEAD
 };
 
 $(window).scroll(posicionarMenu);
 
 
 
-=======
-}
->>>>>>> 7e4eb7d1d8cc773c4c0c3e44ae328e6921103f4b
